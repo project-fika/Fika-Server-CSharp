7c62d128f67960ee72470f11ad23c496be6b3c76
Archangel <jesse@archangel.wtf>
2025-02-13 08:15:34 +0100

    Add json property

--- FikaServer/Models/Fika/Routes/Client/Check/VersionCheckResponse.cs
+++ FikaServer/Models/Fika/Routes/Client/Check/VersionCheckResponse.cs
@@ -1,7 +1,10 @@
-﻿namespace FikaServer.Models.Fika.Routes.Client.Check
+﻿using System.Text.Json.Serialization;
+
+namespace FikaServer.Models.Fika.Routes.Client.Check
 {
     public record VersionCheckResponse
     {
+        [JsonPropertyName("version")]
         public string Version { get; set; } = string.Empty;
     }
 }
