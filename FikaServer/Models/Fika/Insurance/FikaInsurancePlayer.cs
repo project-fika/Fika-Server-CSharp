c3a4fe1a89cce372161d7d683cdc1257692bc5ed
Archangel <jesse@archangel.wtf>
2025-05-04 16:40:21 +0200

    Add missing record

---
+++ FikaServer/Models/Fika/Insurance/FikaInsurancePlayer.cs
@@ -0,0 +1,11 @@
+﻿namespace FikaServer.Models.Fika.Insurance
+{
+    public record FikaInsurancePlayer
+    {
+        public string SessionID { get; set; } = string.Empty;
+        public bool EndedRaid { get; set; } = false;
+        public List<string?> LostItems { get; set; } = [];
+        public List<string?> FoundItems { get; set; } = [];
+        public List<string?> Inventory { get; set; } = [];
+    }
+}
