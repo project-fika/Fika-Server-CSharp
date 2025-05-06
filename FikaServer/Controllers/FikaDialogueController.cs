e76d9618a1e22c76081f483a7fe38a9f30ee2c28
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-05-06 09:10:16 +0200

    Start work on friends & dialogues

---
+++ FikaServer/Controllers/FikaDialogueController.cs
@@ -0,0 +1,68 @@
+﻿using FikaServer.Helpers;
+using FikaServer.Services.Cache;
+using SPTarkov.Common.Annotations;
+using SPTarkov.Server.Core.Controllers;
+using SPTarkov.Server.Core.Helpers;
+using SPTarkov.Server.Core.Models.Eft.Dialog;
+using SPTarkov.Server.Core.Models.Eft.Profile;
+using SPTarkov.Server.Core.Servers;
+
+namespace FikaServer.Controllers
+{
+    [Injectable]
+    public class FikaDialogueController(PlayerRelationsService playerRelationsService, DialogueController dialogueController,
+        ProfileHelper profileHelper, PlayerRelationsHelper playerRelationsHelper, SaveServer saveServer)
+    {
+        public GetFriendListDataResponse GetFriendsList(string profileId)
+        {
+            //var botsAndFriends = dialogueController.GetActiveChatBots();
+            var botsAndFriends = new List<UserDialogInfo>();
+
+            var friends = playerRelationsHelper.GetFriendsList(profileId);
+
+            foreach (var friend in friends)
+            {
+                var profile = profileHelper.GetPmcProfile(friend);
+                if (profile == null)
+                {
+                    playerRelationsHelper.RemoveFriend(profileId, friend);
+                    continue;
+                }
+
+                botsAndFriends.Add(new()
+                {
+                    Id = profile.Id,
+                    Aid = profile.Aid,
+                    Info = new()
+                    {
+                        Nickname = profile.Info.Nickname,
+                        Level = profile.Info.Level,
+                        Side = profile.Info.Side,
+                        MemberCategory = profile.Info.MemberCategory,
+                        SelectedMemberCategory = profile.Info.MemberCategory
+                    }
+                });
+            }
+
+            return new()
+            {
+                Friends = botsAndFriends,
+                Ignore = playerRelationsHelper.GetIgnoreList(profileId),
+                InIgnoreList = playerRelationsHelper.GetInIgnoreList(profileId)
+            };
+        }
+
+        public string SendMessage(string profileId, SendMessageRequest sendMessageRequest)
+        {
+            var profiles = saveServer.GetProfiles();
+            if (!profiles.TryGetValue(profileId, out var profile))
+            {
+                // it's not a player, let SPT handle it
+                return dialogueController.SendMessage(profileId, sendMessageRequest);
+            }
+
+
+            return string.Empty;
+        }
+    }
+}
---
+++ FikaServer/Helpers/PlayerRelationsHelper.cs
@@ -0,0 +1,33 @@
+﻿using FikaServer.Models.Fika;
+using FikaServer.Services.Cache;
+using SPTarkov.Common.Annotations;
+
+namespace FikaServer.Helpers
+{
+    [Injectable]
+    public class PlayerRelationsHelper(PlayerRelationsService playerRelationsService)
+    {
+        public List<string> GetFriendsList(string profileId)
+        {
+            return playerRelationsService.GetStoredValue(profileId).Friends;
+        }
+
+        public void RemoveFriend(string sessionId, string friend)
+        {
+            // TODO: Add
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetIgnoreList(string profileId)
+        {
+            // TODO: Add
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetInIgnoreList(string profileId)
+        {
+            // TODO: Add
+            throw new NotImplementedException();
+        }
+    }
+}
