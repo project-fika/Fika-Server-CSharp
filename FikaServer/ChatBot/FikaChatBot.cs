fc5830aadb4881405b878729716eedc570836a77
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-07-08 16:36:43 +0200

    Add bot base

---
+++ FikaServer/ChatBot/FikaChatBot.cs
@@ -0,0 +1,41 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.Helpers.Dialog.Commando;
+using SPTarkov.Server.Core.Helpers.Dialogue;
+using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Profile;
+using SPTarkov.Server.Core.Models.Enums;
+using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Services;
+
+namespace FikaServer.ChatBot
+{
+    [Injectable]
+    public class FikaChatBot(ISptLogger<AbstractDialogChatBot> Logger, MailSendService MailSendService,
+        ServerLocalisationService localisationService, IEnumerable<IChatCommand> chatCommands)
+        : AbstractDialogChatBot(Logger, MailSendService, localisationService, chatCommands)
+    {
+        private static readonly MongoId _id = new("686d2c4165a0857987a7f1b8");
+
+        public override UserDialogInfo GetChatBot()
+        {
+            return new UserDialogInfo
+            {
+                Id = _id,
+                Aid = 1337,
+                Info = new()
+                {
+                    Level = 1,
+                    MemberCategory = MemberCategory.Emissary,
+                    SelectedMemberCategory = MemberCategory.Emissary,
+                    Nickname = "Mr.Fika",
+                    Side = "Usec",
+                },
+            };
+        }
+
+        protected override string GetUnrecognizedCommandMessage()
+        {
+            return "Unknown command! Type \"help\" to see available commands.";
+        }
+    }
+}
