e900945662bb40a7e91145fb53027436f577cd53
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-10-07 19:11:49 +0200

    Update trefs

--- FikaServer/FikaModMetadata.cs
+++ FikaServer/FikaModMetadata.cs
@@ -8,11 +8,11 @@ public record FikaModMetadata : AbstractModMetadata
     public override string Author { get; init; } = "Fika";
     public override List<string>? Contributors { get; init; } = [];
     public override List<string>? Incompatibilities { get; init; } = [];
-    public override Dictionary<string, SemanticVersioning.Version>? ModDependencies { get; init; } = [];
+    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = [];
     public override string? Url { get; init; } = "https://github.com/project-fika/Fika-Server";
     public override bool? IsBundleMod { get; init; } = false;
     public override string License { get; init; } = "CC-BY-NC-SA-4.0";
     public override string ModGuid { get; init; } = "Fika";
     public override SemanticVersioning.Version Version { get; init; } = new(4, 0, 0);
-    public override SemanticVersioning.Version SptVersion { get; init; } = new(4, 0, 0);
+    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
 }
