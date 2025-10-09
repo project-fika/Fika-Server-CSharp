f1d4a754b83e9cecaf7e6ad09b331f06cca21da9
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-10-09 21:54:45 +0200

    Use property

--- FikaServer/API/GetStatisticsController.cs
+++ FikaServer/API/GetStatisticsController.cs
@@ -23,31 +23,31 @@ public class GetStatisticsController(SaveServer saveServer) : ControllerBase
             {
                 Nickname = player.CharacterData.PmcData.Info.Nickname,
                 Kills = (player.CharacterData?.PmcData?.Stats?.Eft?.OverallCounters?.Items?
-                    .Where(x => x.Key?.Count() == 1 && x.Key.Contains("Kills"))
+                    .Where(x => x.Key?.Count == 1 && x.Key.Contains("Kills"))
                     .Select(x => x.Value)
                     .FirstOrDefault() ?? 0.0),
                 Deaths = (player.CharacterData?.PmcData?.Stats?.Eft?.OverallCounters?.Items?
-                    .Where(x => x.Key?.Count() == 1 && x.Key.Contains("Deaths"))
+                    .Where(x => x.Key?.Count == 1 && x.Key.Contains("Deaths"))
                     .Select(x => x.Value)
                     .FirstOrDefault() ?? 0.0),
                 AmmoUsed = (player.CharacterData?.PmcData?.Stats?.Eft?.OverallCounters?.Items?
-                    .Where(x => x.Key?.Count() == 1 && x.Key.Contains("AmmoUsed"))
+                    .Where(x => x.Key?.Count == 1 && x.Key.Contains("AmmoUsed"))
                     .Select(x => x.Value)
                     .FirstOrDefault() ?? 0.0),
                 BodyDamage = (player.CharacterData?.PmcData?.Stats?.Eft?.OverallCounters?.Items?
-                    .Where(x => x.Key?.Count() == 1 && x.Key.Contains("CauseBodyDamage"))
+                    .Where(x => x.Key?.Count == 1 && x.Key.Contains("CauseBodyDamage"))
                     .Select(x => x.Value)
                     .FirstOrDefault() ?? 0.0),
                 ArmorDamage = (player.CharacterData?.PmcData?.Stats?.Eft?.OverallCounters?.Items?
-                    .Where(x => x.Key?.Count() == 1 && x.Key.Contains("CauseArmorDamage"))
+                    .Where(x => x.Key?.Count == 1 && x.Key.Contains("CauseArmorDamage"))
                     .Select(x => x.Value)
                     .FirstOrDefault() ?? 0.0),
                 Headshots = (player.CharacterData?.PmcData?.Stats?.Eft?.OverallCounters?.Items?
-                    .Where(x => x.Key?.Count() == 1 && x.Key.Contains("HeadShots"))
+                    .Where(x => x.Key?.Count == 1 && x.Key.Contains("HeadShots"))
                     .Select(x => x.Value)
                     .FirstOrDefault() ?? 0.0),
                 BossKills = (player.CharacterData?.PmcData?.Stats?.Eft?.OverallCounters?.Items?
-                    .Where(x => x.Key?.Count() == 1 && x.Key.Contains("KilledBoss"))
+                    .Where(x => x.Key?.Count == 1 && x.Key.Contains("KilledBoss"))
                     .Select(x => x.Value)
                     .FirstOrDefault() ?? 0.0),
             };
