ca1cdcec89e2db6a9cf395c63ca55b0efa612fe6
trippyone <daproblematik@gmail.com>
2026-01-12 13:55:46 -0500

    Changes to allow connecting to an external Nat punch server.

--- FikaServer/OnLoad/FikaPostLoad.cs
+++ FikaServer/OnLoad/FikaPostLoad.cs
@@ -14,7 +14,7 @@ namespace FikaServer.OnLoad;
 
 [Injectable(TypePriority = OnLoadOrder.PostSptModLoader)]
 
-public class FikaPostLoad(ISptLogger<FikaPostLoad> logger, ConfigServer configServer, /*NatPunchServer natPunchServer,*/ ImageRouter imageRouter,
+public class FikaPostLoad(ISptLogger<FikaPostLoad> logger, ConfigServer configServer, ImageRouter imageRouter,
     HeadlessProfileService HeadlessProfileService, LocaleService localeService, PlayerRelationsService playerRelationsCacheService,
     FriendRequestsService friendRequestsService, ConfigService fikaConfig, NatPunchServer natPunchServer, WebhookService webhookService) : IOnLoad
 {
@@ -22,7 +22,7 @@ public class FikaPostLoad(ISptLogger<FikaPostLoad> logger, ConfigServer configSe
     {
         FikaConfig config = fikaConfig.Config;
 
-        if (config.NatPunchServer.Enable)
+        if (config.NatPunchServer.Enable && config.NatPunchServer.Ip == "")
         {
             natPunchServer.Start();
         }
